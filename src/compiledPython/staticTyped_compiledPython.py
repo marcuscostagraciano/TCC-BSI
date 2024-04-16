2c581d3700e10f8888ae21d4618a2bd1f2b4a067
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-16 12:24:17 -0300

    fix: correct typing

--- src/compiledPython/staticTyped_compiledPython.py
+++ src/compiledPython/staticTyped_compiledPython.py
@@ -1,11 +1,11 @@
 import cython
 
 
-def fibonacci_compiled_python(number: cython.int) -> cython.double:
+def fibonacci_compiled_python(number: cython.int) -> cython.ulong:
     """ Fibonacci implementation in default CPython and compiled after with
         static typing """
-    a: cython.double = 0
-    b: cython.double = 1
+    a: cython.ulong = 0
+    b: cython.ulong = 1
 
     for i in range(number):
         a, b = a + b, a
--- src/pureCython/pureCython.pyx
+++ src/pureCython/pureCython.pyx
@@ -2,8 +2,8 @@
 def fibonacci_pure_cython(int n):
     """ Compiled and typed Cython code """
     cdef int i
-    cdef double a = 0
-    cdef double b = 1
+    cdef unsigned long a = 0
+    cdef unsigned long b = 1
 
     for i in range(n):
         a, b = a + b, a
