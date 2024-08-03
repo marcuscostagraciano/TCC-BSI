a8ffe23cdb3a72a711cd4171091e72af40b012b6
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-08-03 16:13:25 -0300

    FIX: remvoe compiled python and rename purePython

--- src/compiledPython/__init__.py
+++
@@ -1,2 +0,0 @@
-from .compiledPython import fibonacci_compiled_python as compiledPython_wo_staticTyping
-from .staticTyped_compiledPython import fibonacci_compiled_python as compiledPython_w_staticTyping
