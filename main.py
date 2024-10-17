2ce259bd7b5ecb4ceccab96928720d3d1b1c4c86
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-10-17 16:57:27 -0300

    TEST: execution of tests

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import time
 from typing import Final
 
 import numpy as np
-from numpy import ndarray
 
-NUM_REGISTROS: Final[int] = 4.5 * 10**9
+from src import get_max_value_C
 
-# start_time = time.perf_counter()
-# # ARRAY_NUMEROS: ndarray = np.arange(NUM_REGISTROS, dtype=np.uint16)
-# ARRAY_NUMEROS: ndarray = np.arange(NUM_REGISTROS, dtype=np.uint32)
-# stop_time = time.perf_counter()
-# print(f"Elapsed ARANGE time: {stop_time - start_time:.3f}s")
+# NUM_REGISTROS: Final[int] = 4.5 * 10**9
+NUM_REGISTROS: Final[int] = 2.5 * 10**9
 
-# start_time = time.perf_counter()
-# min_value_from_array = np.min(ARRAY_NUMEROS)
-# stop_time = time.perf_counter()
-# print(f"Elapsed MIN time: {stop_time - start_time:.3f}s")
+start_time = time.perf_counter()
+ARRAY_NUMEROS = np.arange(NUM_REGISTROS, dtype=np.uint32)
+end_time = time.perf_counter()
 
-# start_time = time.perf_counter()
-# max_value_from_array = np.max(ARRAY_NUMEROS)
-# stop_time = time.perf_counter()
-# print(f"Elapsed MAX time: {stop_time - start_time:.3f}s")
+print(f"{ARRAY_NUMEROS.size = :,}")
+print(f"Array creation timing: {end_time - start_time:.5f}s\n")
 
-from src.pureCython import create_array_wrapper
+start_time = time.perf_counter()
+get_max_value_C_result = get_max_value_C(ARRAY_NUMEROS)
+end_time = time.perf_counter()
 
-# from src.pureCython.handle_array import create_array_wrapper
+print(f"get_max_value_C timing: {end_time - start_time:.5f}s")
+print(f"get_max_value_C result: {get_max_value_C_result}")
 
-create_array_wrapper(NUM_REGISTROS)
+start_time = time.perf_counter()
+get_max_value_numpy_result = np.max(ARRAY_NUMEROS)
+end_time = time.perf_counter()
+
+print(f"get_max_value_numpy timing: {end_time - start_time:.5f}s")
+print(f"get_max_value_numpy result: {get_max_value_numpy_result}")
