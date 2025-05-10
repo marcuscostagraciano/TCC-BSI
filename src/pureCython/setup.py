3e438431f7f5359b04b7f39e2273ecfa0e1fa771
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2025-05-10 17:30:55 -0300

    DOC

--- src/pureCython/setup.py
+++ src/pureCython/setup.py
@@ -1,35 +1,42 @@
 # compile files: python ./src/pureCython/setup.py build_ext --inplace
 # clean files: python ./src/pureCython/setup.py clean --all
-# clean files: python ./src/pureCython/setup.py clean --all
 
 import os
 from typing import Final
 
-import numpy as np
 from Cython.Build import cythonize
+from numpy import get_include
 from setuptools import setup
 from setuptools.extension import Extension
 
+# Altera o diretório para onde estão os módulos Cython
 os.chdir("./src/pureCython/")
-
+# Tupla com nomes de pastas que devem ser ignoradas
 FOLDER_FILTER: Final[tuple[str]] = ("build", ".", "__pycache__")
-
+# Lista todas as pastas no diretório atual, exceto as que estão em FOLDER_FILTER
 CYTHON_FOLDERS: Final[list[str]] = [
     path for path in os.listdir()
     if not any(folder_filter in path for folder_filter in FOLDER_FILTER)
 ]
 
-print(CYTHON_FOLDERS)
+print(f"{CYTHON_FOLDERS = }")
 
-ext_modules = []
+# Cria uma lista para armazenar os módulos de extensão
+ext_modules: list[Extension] = []
 for folder in CYTHON_FOLDERS:
+    # Para cada pasta, cria uma extensão Cython
     ext_modules.append(
         Extension(
+            # Nome do módulo
             folder,
+            # Arquivos fonte .pyx dentro da pasta
             [f"./{folder}/" + "*.pyx"],
-            include_dirs=[np.get_include()],
+            # Inclui diretório de headers do numpy
+            include_dirs=[get_include()],
+            # Linguagem alvo (C)
             language="c",
         )
     )
 
+# Executa o setup, compilando os módulos Cython encontrados
 setup(ext_modules=cythonize(ext_modules))
